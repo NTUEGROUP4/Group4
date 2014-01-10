291e2299c9e92cfd9ef26c4ae1ec543602bc5dba
henry <hhenry83412@gmail.com>
2014-01-10 11:36:32 +0800

    final

--- manager_login.php
+++ manager_login.php
@@ -7,18 +7,18 @@
 
 <body>
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<h2>數學學習網</h2>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><a href="reading.php">開始閱讀</a></li>   
-		<li><a href="upload.php">檔案上傳</a></li>
+		<li><a href="reading.php">會員登入</a></li>   
+		<li><a href="upload.php">好康分享</a></li>
 		<li><a href="message.php">留言板</a></li>
 		<li><a href="record.php">歷史紀錄</a></li>
 		<li><a href="login.php">會員資料修改</a	></li>
-		<li><a href="group.php">管理團隊</a></li>
+		<li><a href="group.php">關於我們</a></li>
 		<li><b>管理者專區</b></li>
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
+		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>//旁邊的列表
 	</ul>
 </div>
 <div id="CONTENT">
@@ -33,7 +33,7 @@
 			帳號：<input type="text" name="id" /> <br>
 			密碼：</h1><input type="password" name="pw" /> <br>
 			</p>
-			<input type="submit" name="button" value="送出" />
+			<input type="submit" name="button" value="送出" />//帳號密碼輸入
 			<p>
 			</p>
 		</form>
@@ -43,8 +43,9 @@
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
+		<h2><center><br/>Author by <i>Group4</i> </center></h2>
 	</p>
 </div>
 </body>
 </html>
+
