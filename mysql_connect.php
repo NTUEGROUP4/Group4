7dee5a3821f475c52a9d845138cfa80a065eeaeb
henry <hhenry83412@gmail.com>
2014-01-10 11:39:18 +0800

    final

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,8 +1,8 @@
 <?php  
 $host="localhost";
-$user="root";
-$password="NTUE";
-$link=mysql_connect($host,$user,$password);
+$user="group4";
+$password="cd5";
+$link=mysql_connect($host,$user,$password);//帳號和密碼的紀錄
 $query = "SET NAMES 'utf8'";
 $result = mysql_query($query);
 mysql_select_db("Group4",$link) || die("db error");
